b821c8a36c612707d0b1846513255e88d43aa4e6
trancanh277 <tranminhcanh.tp@gmail.com>
2025-08-24 23:04:51 +0900

    upload session 01 bai tap  4

---
+++ baitap04.java
@@ -0,0 +1,12 @@
+public class baitap04 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 2;
+        int tong = a + b;
+        int hieu = a - b;
+        int tich = a * b;
+        int thuong = a * b;
+        int chiaDu = a % b;
+        System.out.printf("a = %d, và b = %d%n Tổng 2 số a + b = %d%n Hiệu 2 số a - b = %d%n Tích 2 số a * b = %d%n Thương 2 số a / b = %d%n Số dư khi chia số a cho b = %d", a, b, tong, hieu, tich, thuong, chiaDu);
+    }
+}
