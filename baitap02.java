3a4e4dfed2dd863dcb93e140c0ed03d91e873e18
trancanh277 <tranminhcanh.tp@gmail.com>
2025-08-24 22:36:32 +0900

    upload session 01 bai tap 2

---
+++ baitap02.java
@@ -0,0 +1,9 @@
+public class baitap02 {
+    public static void main(String[] args) {
+        String name = "Tran Minh Canh";
+        int age = 30;
+        String address = "Phu Yen";
+        System.out.printf("Tôi tên là %s, tôi %d tuổi, hiện đang sống tại Nhật Bản. Quê ở %s.%n", name, age, address);
+
+    }
+}
